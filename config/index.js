ca8aa29c94a1f3493d6492c3e6634a907677a770
emanuelosva <emanuelosva@gmail.com>
2020-06-29 13:25:19 -0500

    env config

---
+++ config/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+const dbCluster = process.env.DB_CLUSTER;
+
+module.exports = {
+  port: process.env.PORT || 3000,
+  db: {
+    URI: `mongodb+srv://${dbUser}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority`,
+  },
+  env: {
+    node_env: process.env.NODE_ENV || 'development',
+    dev: process.env.NODE_ENV !== 'production',
+  },
+};
